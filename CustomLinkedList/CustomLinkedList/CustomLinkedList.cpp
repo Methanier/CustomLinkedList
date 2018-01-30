34d623b1edcb90ddbf8a733e28549b1df0e763a9
Charles Cozart <CHACOZAR@uat.edu>
2018-01-30 11:14:05 -0700

    init project

---
+++ CustomLinkedList/CustomLinkedList/CustomLinkedList.cpp
@@ -0,0 +1,14 @@
+// CustomLinkedList.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+	system("pause");
+}
+
