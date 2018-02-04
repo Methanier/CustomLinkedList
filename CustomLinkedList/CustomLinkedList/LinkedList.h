b2d79f07ce75415ae9620f7d215b6854a9bd6788
Charles Cozart <charlescozart24@gmail.com>
2018-02-04 15:17:15 -0700

    Finished header file

--- CustomLinkedList/CustomLinkedList/LinkedList.h
+++ CustomLinkedList/CustomLinkedList/LinkedList.h
@@ -25,10 +25,44 @@ private:
 		Node(T && inData, Node * next = nullptr, Node * prev = nullptr) : data{ std::move(indata) }, nextItem{ next }, prevItem{ prev } {}
 	};
 
-	private:
-		int listSize;
-		DataNode *head;
-		DataNode *tail;
+public:
+	//Empty Constructor creates empty linked list
+	LinkedList();
+
+	//Constructor to copy a const LinkedList into this list
+	LinkedList(const LinkedList & inList);
+
+	//Constuctor to copy a non-const LinkedList into this list
+	LinkedList(LinkedList && inList);
+
+	//Explicity destructor clears all items in the linked list, then deletes the head and tail pointers
+	~LinkedList();
+
+	//Insert method to insert a const value of type T into the list
+	void Insert(const T & inData);
+
+	//Insert method to insert a non-const value of type T into the list
+	void Insert(T && inData);
+
+	//Find method to find a const value of type T in the list
+	void Find(const T & inData);
+
+	//Find method to find a non-sont value of type T in the list
+	void Find(T && inData);
+
+	//Delete method to remove a const value of type T from the list
+	void Delete(const T & inData);
+	
+	//Delete method to remove a non-const value of type T from the list
+	void Delete(T & inData);
+
+	//Initalises all the values for an empty list
+	void Init();
+
+private:
+	int listSize;
+	DataNode *head;
+	DataNode *tail;
 
 };
 
